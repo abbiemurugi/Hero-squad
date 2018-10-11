4be292751ba1297e5707724f12ba092349150969
abbiemurugi <abigaelmurugi@gmail.com>
2018-10-11 12:06:47 +0300

    added hero-squad class

---
+++ src/main/java/HeroSquad.java
@@ -0,0 +1,13 @@
+public class HeroSquad {
+    private String myDescription;
+
+    public HeroSquad(String description) {
+        myDescription = description;
+
+    }
+
+    public String getDescription() {
+        return myDescription;
+    }
+
+}
