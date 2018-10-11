a33f74e17ed09dc02796fe51d79959c82db4418a
abbiemurugi <abigaelmurugi@gmail.com>
2018-10-11 10:03:48 +0300

    added routes for forms and images

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ import static spark.Spark.*;
 import java.util.HashMap;
 import java.util.Map;
 import spark.ModelAndView;
-import spark.template.velocity.VelocityTemplateEngine;
+//import spark.template.velocity.VelocityTemplateEngine;
 
 public class App {
     public static void main(String[] args) {
@@ -21,8 +21,17 @@ public class App {
 //end heroku
 
         get("/", (request, response) -> {
-            return new ModelAndView(new HashMap(), "templates/hero.vtl");
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/hero.vtl" );
+            return new ModelAndView(model, layout);
                 }, new VelocityTemplateEngine());
+
+        get("/heroes", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/heroes.vtl" );
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
         get("/form", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             model.put("template", "templates/form.vtl");
@@ -30,4 +39,5 @@ public class App {
         }, new VelocityTemplateEngine());
 
     }
-}"<p><a href='/heros' >P.S. Check out my hero photos here.</a></p>" +
+}
+// "<p><a href='/heros' >P.S. Check out my hero photos here.</a></p>" +
